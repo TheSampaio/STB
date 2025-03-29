98188914af0868120ebe625a697f867a9d26a2f5
Kellvyn Sampaio <mirandasampaiokellvyn@outlook.com>
2025-03-29 15:46:03 -0300

    Added Premake5
    • Added "premake5.lua" file

---
+++ premake5.lua
@@ -0,0 +1,10 @@
+project "stb"
+    kind "StaticLib"
+    language "C++"
+    location "build"
+    
+    targetdir ("out/build/bin/%{cfg.buildcfg}/%{prj.name}")
+    objdir    ("out/build/obj/%{cfg.buildcfg}/%{prj.name}")
+
+    includedirs { "include" }
+    files { "source/stb_image.cpp", "source/stb_truetype.cpp" }
