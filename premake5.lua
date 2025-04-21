67ffcabcda40038cc15ac049962a5a2b512c6931
Kellvyn Sampaio <mirandasampaiokellvyn@outlook.com>
2025-04-21 01:27:32 -0300

    Build System Improved

--- premake5.lua
+++ premake5.lua
@@ -3,8 +3,8 @@ project "stb"
     language "C++"
     location "build"
     
-    targetdir ("out/build/bin/%{cfg.buildcfg}/%{prj.name}")
-    objdir    ("out/build/obj/%{cfg.buildcfg}/%{prj.name}")
+    targetdir (".Out/Bin/" .. output_dir .. "%{prj.name}")
+    objdir    (".Out/Obj/" .. output_dir .. "%{prj.name}")
 
     includedirs { "include" }
     files { "source/stb_image.cpp", "source/stb_truetype.cpp" }
